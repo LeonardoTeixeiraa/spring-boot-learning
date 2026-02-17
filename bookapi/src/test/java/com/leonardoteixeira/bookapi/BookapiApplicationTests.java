6fddf3051ad06d8d64c06a7a228b56b9299b53d5
LeonardoTeixeiraa <leonardo.teixeira@aluno.ifsp.edu.br>
2026-02-16 22:49:01 -0300

    initial commit

---
+++ bookapi/src/test/java/com/leonardoteixeira/bookapi/BookapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonardoteixeira.bookapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
